8f9e00eb8cb21be62bb41a63f6661e54ab4a1d88
PRASHANTHANSDA2002 <110048717+PRASHANTHANSDA2002@users.noreply.github.com>
2023-07-07 14:39:08 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
     e.preventDefault();
     setLoading(true);
     axios
-      .post("http://localhost:5555/chat", { prompt })
+      .post("https://chatbot-o8ew.onrender.com/chat", { prompt })
       .then((res) => {
         setResponse(res.data);
         setLoading(false);
